0c12724430c448608f4fe505df9960258625f2ec
michelle <michelle@civicpatch.org>
2025-11-12 13:29:09 -0800

    Reorganize scripts

--- scripts/one_off/count_jurisdictions.py
+++ scripts/one_off/count_jurisdictions.py
@@ -1,16 +1,21 @@
-import yaml
 import sys
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).parent.parent
+import yaml
+
+PROJECT_ROOT = Path(__file__).parent.parent.parent.parent
+
 
 def count_jurisdictions(state: str):
     print("Counting jurisdictions...")
     jurisdictions_file_path = PROJECT_ROOT / "data_source" / state / "jurisdictions.yml"
-    jurisdictions_data = yaml.safe_load(jurisdictions_file_path.read_text(encoding="utf-8"))
+    jurisdictions_data = yaml.safe_load(
+        jurisdictions_file_path.read_text(encoding="utf-8")
+    )
 
     num_jurisdictions = len(jurisdictions_data["jurisdictions"])
     print(f"Number of jurisdictions in {state}: {num_jurisdictions}")
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
