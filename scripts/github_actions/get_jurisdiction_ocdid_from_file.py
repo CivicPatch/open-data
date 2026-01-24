4405d70a7300bb3616cbc7530e8cae374bb32406
michelle@shelltr.me <michelle@shelltr.me>
2026-01-23 16:00:39 -0800

    Move CDN updates to after pull request merge

---
+++ scripts/github_actions/get_jurisdiction_ocdid_from_file.py
@@ -0,0 +1,33 @@
+import yaml
+import sys
+
+def get_jurisdiction_ocdid_from_file(file_path: str) -> str | None:
+    """Extract the jurisdiction_ocdid from a given YAML file.
+
+    Args:
+        file_path (str): The path to the YAML file, which is an array of People.
+
+    Returns:
+        str | None: The jurisdiction_ocdid of the first person entry if found, otherwise None.
+    """
+    with open(file_path, 'r') as file:
+        data = yaml.safe_load(file)
+        if data and isinstance(data, list):
+            return data[0].get('jurisdiction_ocdid', None)
+        return None
+    
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) <= 1:
+        print("Usage: python get_jurisdiction_ocdid_from_file.py <file_path>, <file_path>")
+        sys.exit(1)
+    file_paths_arg = sys.argv[1:]
+    #print(f"Received argument: {file_paths_arg}")
+    file_paths = [path for path in file_paths_arg[0].split(",")]
+    #print(f"Processing files: {file_paths}")
+    ocdids = []
+    for file_path in file_paths:
+        ocdid = get_jurisdiction_ocdid_from_file(file_path)
+        if ocdid:
+            ocdids.append(ocdid)
+    print(",".join(ocdids))
