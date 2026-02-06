8b79b45633654749ce3d79a75861a0b19118288d
michelle@shelltr.me <michelle@shelltr.me>
2026-02-06 15:42:51 -0800

    Switch to include log url

--- scripts/github_actions/pull_request_body_markdown.py
+++ scripts/github_actions/pull_request_body_markdown.py
@@ -2,8 +2,10 @@ import sys
 from scripts.utils import jurisdiction_ocdid_to_folder
 import os
 import yaml
+import json
+import requests
 
-def pull_request_body_markdown(jurisdiction_ocdid: str, request_id: str, workflow_context_url: str) -> str:
+def pull_request_body_markdown(jurisdiction_ocdid: str, request_id: str, log_url: str) -> str:
     """
     Generate a markdown-formatted pull request body for updating a jurisdiction's OCDID.
 
@@ -18,9 +20,11 @@ def pull_request_body_markdown(jurisdiction_ocdid: str, request_id: str, workflo
 
     jurisdiction_folder = jurisdiction_ocdid_to_folder(jurisdiction_ocdid)
     jurisdiction_data_source_folder = os.path.join("data_source", jurisdiction_folder)
-    config_file = os.path.join(jurisdiction_data_source_folder, "config.yml")
-    with open(config_file, "r") as f:
-        config = yaml.safe_load(f)
+    workflow_context_path = os.path.join(jurisdiction_data_source_folder, "workflow_context.json")
+    with open(workflow_context_path, "r") as f:
+        workflow_context = json.load(f)
+
+    config = workflow_context.get("data", {}).get("format_output_step", {}).get("config", {})
 
     # Extract relevant information from the config
     jurisdiction_url = config.get("url", "N/A")
@@ -46,21 +50,20 @@ Note: some configs, like source_urls and identities, are generated after the scr
 
 ## Request Information
 - **Request ID**: {request_id}
-- **Workflow Context URL**: [Link]({workflow_context_url})
+- **Log URL**: [Link]({log_url})
 
 """
 
 def markdown_list_br(items):
     return "<br>".join(f"- {item}" for item in items) if items else "N/A"
 
-
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: python pull_request_body_markdown.py <jurisdiction_ocdid> <request_id> <workflow_context_url>")
+        print("Usage: python pull_request_body_markdown.py <jurisdiction_ocdid> <request_id> <log_url>")
         sys.exit(1)
 
     jurisdiction_ocdid = sys.argv[1]
     request_id = sys.argv[2]
-    zip_file_debug_url = sys.argv[3]
-    pr_body = pull_request_body_markdown(jurisdiction_ocdid, request_id, zip_file_debug_url)
+    log_url = sys.argv[3]
+    pr_body = pull_request_body_markdown(jurisdiction_ocdid, request_id, log_url)
     print(pr_body)
